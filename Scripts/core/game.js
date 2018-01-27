b4032efaba9d484fa9efd49e4e71fd9cfb90652a
asikalev <300783815@cencol.ca>
2018-01-27 17:46:57 -0500

    added npm support

---
+++ Scripts/core/game.js
@@ -0,0 +1,25 @@
+// IIFE - Immediatly Invoked Function Expression
+(function(){
+
+    function Init(){
+        console.log("Initalization Started");
+
+        let myVariable = "foo";
+        let myVariable = "1";
+    }
+
+    function Start(){
+
+    }
+
+    function Update(){
+
+    }
+
+    function Main(){
+
+    }
+
+    window.onload = Init;
+
+})();
